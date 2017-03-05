7f2f447bae074d3708d3fae59b18de33694cb9cd
Haris Jamal <harisjamal2k13@gmail.com>
2017-03-05 17:46:26 +0500

    If and Else Assignment

---
+++ app.js
@@ -0,0 +1,20 @@
+
+var a = prompt("What is Your Percentage? (number only)");
+if (a >= 90 ) {
+    alert("Superb");
+}
+else if (a >= 80) {
+    alert("A-1");
+}
+else if (a >= 70) {
+    alert("A");
+}
+else if (a >= 60) {
+    alert("B");
+}
+else if (a >= 50) {
+    alert("D");
+}
+else {
+    alert("Fail");
+}
